50950671dd9689f4d1dfbfb2cdeed7647bf5891d
Jeff Philapy <jeff@altahost.com>
2021-01-27 14:08:05 -0500

    adding new pages, and templates

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'AdminController@index')->name('dashboard');
